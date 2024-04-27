f5fe605fc4a7ed55bfc7f143e870deb947eb2369
DavidDIaz0504 <deyvidsoz770@gmail.com>
2024-04-27 18:01:14 -0500

    register done home page

--- app/src/main/java/david/alejandro/projectecomm/login_activity.java
+++ app/src/main/java/david/alejandro/projectecomm/login_activity.java
@@ -1,6 +1,10 @@
 package david.alejandro.projectecomm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class login_activity extends AppCompatActivity {
+    Button btnIniciarSesion;
+    TextView tvRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnIniciarSesion = findViewById(R.id.btn_iniciar_sesion);
+        tvRegistrarse = findViewById(R.id.tv_registrarse);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad "activity_main"
+                startActivity(new Intent(login_activity.this, MainActivity.class));
+                // Finalizar esta actividad
+                finish();
+            }
+        });
+
+        tvRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad "activity_register"
+                startActivity(new Intent(login_activity.this, register_activity.class));
+                // Finalizar esta actividad
+                finish();
+            }
         });
     }
 }
