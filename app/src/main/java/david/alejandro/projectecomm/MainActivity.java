8cbcd49469b03d38bbbe146bff4de87c139c5c37
Propietario <lbeltran598@unab.edu.co>
2024-05-12 16:35:21 -0500

    Coniguracion booton bar y botones

--- app/src/main/java/david/alejandro/projectecomm/MainActivity.java
+++ app/src/main/java/david/alejandro/projectecomm/MainActivity.java
@@ -1,9 +1,11 @@
 package david.alejandro.projectecomm;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -13,6 +15,11 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+//import android.support.design.widget.BottomAppBar;
+import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Product> listProducts = new ArrayList<>();
     private ImageSlider imageSlider;
     private RecyclerView rvProductsMain;
+    private BottomAppBar bnTienda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         //cargar informacion falsa
         loadFakeData();
         //slider de eventos
-        imageSlider= findViewById(R.id.event_slider_home);
+        imageSlider = findViewById(R.id.event_slider_home);
         eventList.add(new SlideModel("https://bit.ly/2YoJ77H", "The animal population decreased by 58 percent in 42 years.", ScaleTypes.FIT));
         eventList.add(new SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct.", ScaleTypes.FIT));
         eventList.add(new SlideModel("https://bit.ly/3fLJf72", "And people do that.", ScaleTypes.FIT));
@@ -41,8 +49,39 @@ public class MainActivity extends AppCompatActivity {
         rvProductsMain.setAdapter(myAdapter);
         StaggeredGridLayoutManager myStaged = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
         rvProductsMain.setLayoutManager(myStaged);
+        //barra de navegacion
+        bnTienda = findViewById(R.id.bottom_navigation_tienda);
+        bnTienda.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        bnTienda.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.page_count) {
+                    // Manejar la pulsación del icono page_count
+                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                    startActivity(intent);
+                } else if (item.getItemId() == R.id.page_scanner) {
+                    // Manejar la pulsación del icono accelerator
+                    Intent intent1 = new Intent(MainActivity.this, ScannerActivity.class);
+                    startActivity(intent1);
+                } else if (item.getItemId() == R.id.page_carrito) {
+                    // Manejar la pulsación del icono rotation
+                    Intent intent2 = new Intent(MainActivity.this, CartShoppingActivity.class);
+                    startActivity(intent2);
+                } else {
+                    // Manejar otros casos
+                }
+                return false;
+            }
+
+        });
     }
+
     private void loadFakeData() {
         Product product1 = new Product("Computador Asus", " El mejor computador Gamer que puedes comprar ", 11000000.0, "https://rampcrosario.com/wp-content/uploads/2019/03/pc-gamer.png");
         Product product2 = new Product("Teclado Asus ", " El mejor teclado Gamer que puedes comprar ", 100000.0, "https://d22fxaf9t8d39k.cloudfront.net/f65ad7c8036f1e99b17e1e3fbcd89625026e26a0e81e4af34b1dc8b0cf7d235c169554.png");
