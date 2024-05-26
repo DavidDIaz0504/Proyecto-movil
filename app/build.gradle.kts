0d1c45a4944959b5e8ff04a13d4ed01839214ca9
Alejandro <lbeltran598@unab.edu.co>
2024-05-26 11:18:39 -0500

    login con autenticacion de firebase
    Signed-off-by: Alejandro <lbeltran598@unab.edu.co>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,12 @@ dependencies {
     implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
     //implementacion de picasso
     implementation ("com.squareup.picasso:picasso:2.8")
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 
 
 }
