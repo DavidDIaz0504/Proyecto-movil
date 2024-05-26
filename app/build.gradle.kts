4e283bd7ac6df7e99b3da9d75e47cf5135529ba3
Alejandro <lbeltran598@unab.edu.co>
2024-05-26 12:53:46 -0500

    registro guardando los datos en firebase
    Signed-off-by: Alejandro <lbeltran598@unab.edu.co>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth")
+    //implementacion de firestore
+    implementation("com.google.firebase:firebase-firestore")
 
 
 }
